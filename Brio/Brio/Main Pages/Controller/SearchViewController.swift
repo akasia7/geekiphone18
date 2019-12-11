801cb1a79b5171fd42de41a2acc40fd9704910e8
maitokuwahara <maito.kuwahara@thebanana.co.jp>
2019-12-11 23:33:30 +0900

    Fix show follow

--- Brio/Brio/Main Pages/Controller/SearchViewController.swift
+++ Brio/Brio/Main Pages/Controller/SearchViewController.swift
@@ -14,7 +14,8 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     
     var users = [NCMBUser]()
     
-    var followingUserIds = [String]()
+    var followingUserIds:[String] = []
+    var followingUser:[NCMBUser] = []
     
     var searchBar: UISearchBar!
     
@@ -39,7 +40,7 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     override func viewWillAppear(_ animated: Bool) {
         loadUsers(searchText: nil)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
@@ -48,7 +49,7 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
     }
-
+    
     func setSearchBar() {
         // NavigationBarにSearchBarをセット
         if let navigationBarFrame = self.navigationController?.navigationBar.bounds {
@@ -78,26 +79,30 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return followingUser.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell") as! SearchTableViewCell
         
-        let userImageUrl = "https://mbaas.api.nifcloud.com/2013-09-01/applications/HlgrPhqq9ECHzZT3/publicFiles/VKD52cVhRFPhLmTY.png"
+        //let userImageUrl = "https://mbaas.api.nifcloud.com/2013-09-01/applications/HlgrPhqq9ECHzZT3/publicFiles/VKD52cVhRFPhLmTY.png"
         
         
-    
+        guard let userImageUrl = followingUser[indexPath.row].object(forKey: "imageUrl") as? String else {
+            return cell
+        }
+        
         cell.userImageView.layer.cornerRadius = cell.userImageView.bounds.width / 2.0
         cell.userImageView.layer.masksToBounds = true
+        cell.userImageView.kf.setImage(with: URL(string: userImageUrl))
         
-        cell.userNameLabel.text = users[indexPath.row].object(forKey: "displayName") as? String
+        cell.userNameLabel.text = followingUser[indexPath.row].object(forKey: "displayName") as? String
         
         // Followボタンを機能させる
         cell.tag = indexPath.row
         cell.delegate = self
         
-        if followingUserIds.contains(users[indexPath.row].objectId) == true {
+        if followingUserIds.contains(followingUser[indexPath.row].objectId) {
             cell.followButton.isHidden = true
         } else {
             cell.followButton.isHidden = false
@@ -108,11 +113,11 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     
     
     func didTapFollowButton(tableViewCell: UITableViewCell, button: UIButton) {
-        let displayName = users[tableViewCell.tag].object(forKey: "displayName") as? String
+        let displayName = followingUser[tableViewCell.tag].object(forKey: "displayName") as? String
         let message = displayName! + "をフォローしますか？"
         let alert = UIAlertController(title: "フォロー", message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
-            self.follow(selectedUser: self.users[tableViewCell.tag])
+            self.follow(selectedUser: self.followingUser[tableViewCell.tag])
         }
         let cancelAction = UIAlertAction(title: "キャンセル", style: .default) { (action) in
             alert.dismiss(animated: true, completion: nil)
@@ -149,20 +154,6 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     
     //ユーザーを読み込む
     func loadUsers(searchText: String?) {
-        guard let searchText = searchText else {
-            return
-        }
-
-        let nCMBQuery = NCMBQuery.init(className: "Follow")
-        nCMBQuery?.addDescendingOrder("createDate")
-        nCMBQuery?.findObjectsInBackground{ items, error in
-            //items?.forEach()
-            print(items)
-        }
-        //let nCMBObject = NCMBObject.init(className: "Follo")
-        //nCMBObject.order
-        //query?.order(byDescending: "createDate")
-        /*
         let query = NCMBUser.query()
         
         // 自分を除外
@@ -170,56 +161,64 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
         
         // 退会済みアカウントを除外
         query?.whereKey("active", notEqualTo: false)
-
+        
         // 検索ワードがある場合
         if let text = searchText {
             query?.whereKey("userName", equalTo: text)
         }
-
+        
         // 新着ユーザー50人だけ拾う
         query?.limit = 50
         
         // 降順にソート
         query?.order(byDescending: "createDate")
-
-        query?.findObjectsInBackground({ (result, error) in
+        
+        query?.findObjectsInBackground {[weak self] result, error in
+            guard let self = self else { return }
             if error != nil {
-                print(error)
-                 SVProgressHUD.showError(withStatus: error!.localizedDescription)
+                //print(error)
+                SVProgressHUD.showError(withStatus: error!.localizedDescription)
             } else {
                 
                 // 取得した新着50件のユーザーを格納
                 self.users = result as! [NCMBUser]
-                print("users")
-                print(self.users)
+                //print("users")
+                //print(self.users)
                 self.loadFollowingUserIds()
             }
-        })
- */
+        }
     }
-
-   
-     
+    
+    
+    
     func loadFollowingUserIds() {
         let query = NCMBQuery(className: "Follow")
         query?.includeKey("user")
         query?.includeKey("following")
         query?.whereKey("user", equalTo: NCMBUser.current())
         
-        query?.findObjectsInBackground({ (result, error) in
+        query?.findObjectsInBackground { [weak self] result, error in
+            guard let self = self else { return }
             if error != nil {
                 SVProgressHUD.showError(withStatus: error!.localizedDescription)
             } else {
-                self.followingUserIds = [String]()
-                for following in result as! [NCMBObject] {
-                    let user = following.object(forKey: "following") as! NCMBUser
-                   
+                self.followingUserIds = []
+                self.followingUser = []
+                result?.forEach { item in
+                    print(item)
+                    guard let nCMBObject = item as? NCMBObject, let user = nCMBObject.object(forKey: "following") as? NCMBUser else {
+                        return
+                    }
+                    self.followingUser.append(user)
+                    self.followingUserIds.append(user.objectId)
                 }
+
                 
                 self.searchUserTableView.reloadData()
                 
             }
-        })
+        }
     }
-
+    
 }
+
